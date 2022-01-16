feff15b82fbdc3320f73b499f2621ef7270a9719
Richard van Esveld <Rfve@me.com>
2022-01-16 10:42:24 +0100

    Sorted function working

--- src/Components/TrackOverview.js
+++ src/Components/TrackOverview.js
@@ -6,11 +6,12 @@ import NewTrack from './NewTrack'
 function TrackOverview() {
     const [tracks, setTracks] = useState({
         allTracks: [
-            { Artist: ' London Gramar ', Title: ' How does it feel ', Genre: ' Pop ', Rating: 5 },
+            { Artist: ' Blof ', Title: ' Aan de Kust ', Genre: ' Dutch ', Rating: 5 },
             { Artist: ' Kensington ', Title: ' Composure ', Genre: ' Pop ', Rating: 3 },
             { Artist: ' Tears for Fears ', Title: ' Shout ', Genre: ' Pop ', Rating: 4 },
-            { Artist: ' Ed Sheeran ', Title: ' Visiting Hours ', Genre: ' Pop ', Rating: 5 },
-            { Artist: ' Imagine Dragons ', Title: ' Natural ', Genre: ' Pop ', Rating: 5 },
+            { Artist: ' Ed Sheeran ', Title: ' Visiting Hours ', Genre: ' Pop ', Rating: 1 },
+            { Artist: ' Imagine Dragons ', Title: ' Natural ', Genre: ' Pop ', Rating: 2 },
+            { Artist: ' Cruel Youth ', Title: ' Black River ', Genre: ' R&B ', Rating: 3 },
         ]
     })
 
@@ -26,19 +27,24 @@ function TrackOverview() {
         setTracks({ allTracks: filteredTracks })
     }
 
-    const sortTracks = (fieldName, reverse) => {
+    const sortTracks = (fieldName, reverse = false) => {
+        console.log(fieldName);
+
+
         let sortedTracks = tracks.allTracks.sort((track1, track2) => {
-            if (track1[fieldName].toLowerCase() > track2[fieldName].toLowerCase()) {
-                return 1
+            if (fieldName === 'Rating') {
+                return track1[fieldName] - track2[fieldName]
+            } else if (track1[fieldName].toLowerCase() > track2[fieldName].toLowerCase()) {
+                return 1;
             }
-            if (track1.track < track2.track) {
-                return -1
+            if (track2[fieldName].toLowerCase() > track1[fieldName].toLowerCase()) {
+                return -1;
             }
-            return 0
-        })
-        reverse && sortedTracks.reverse()
-        setTracks({ allTracks: sortedTracks })
-    }
+            return 0;
+        });
+        reverse && sortedTracks.reverse();
+        setTracks({ allTracks: sortedTracks });
+    };
 
     return (
         <div>
